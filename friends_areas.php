4a68cdb1ec256f3213aee823fd657ddfb80c98a0
yoshinoromanuke <yoshiro.shibata117@gmail.com>
2015-04-28 16:49:09 +0800

    友達がいない都道府県にはリンクを貼らないように処理

--- friends_areas.php
+++ friends_areas.php
@@ -23,33 +23,76 @@ $dbh->query('SET NAMES utf8');
 <body>
 	<?php //echo $_SERVER['HTTP_USER_AGENT']; ?>
 <h1>都道府県友達リスト</h1>
-	<?php
-	//3.SQL文で都道府県リストをデータベースから取得し、表示させる。
-	$sql ='SELECT * FROM `areas`;';
-		//FROMでメインのテーブルを指定。表を結合を左側or右側からを指定し、結合させるテーブルを指定。今回は不要
-		//$sql .='FROM areas LEFT OUTER JOIN friends';
-		//メインのテーブルと結合させるテーブルの何が一致しているのか明示。
-		//$sql .= 'ON areas.id = friends.area_id.areas.id ';
-		//
-		//$sql .= 'GROUP BY areas.id';
+		<?php
+		//2.SQLで指令をだす
+		
+		$sql ='SELECT areas.*,count(friends.id) AS `friends_count` ';
+		$sql .='FROM areas LEFT OUTER JOIN friends';
+		$sql .= ' ON areas.id = friends.area_id ';
+		$sql .= 'GROUP BY areas.id';
+		//var_dump($sql);
 		//echo $sql;
 		$stmt = $dbh->prepare($sql);
 		$stmt->execute();
 		echo '<table style="border:1px;">';
 		while(1){
 			$rec = $stmt->fetch(PDO::FETCH_ASSOC);
-			//var_dump($rec);
-			if ($rec == false) {
+			if ($rec == false) 
+			{
 				break;
 			}
-			echo '<tr>';
-			echo '<td>'.$rec['id'].'</td>';
-			echo '<td><a href="friends_friends.php?area_id='.$rec['id'].'">'.$rec['name'].'</a></td>';
-			echo '</tr>';
-		}
+				if ($rec['friends_count']==0)
+				{
+					echo '<tr>';
+					echo '<td>'.$rec['id'].'</td>';
+					echo '<td>'.$rec['name'].'</td>';
+					echo '<td>'.$rec['friends_count'].'</td>';
+					echo '</tr>';
+				}
+				else
+				{
+					echo '<tr>';
+					echo '<td>'.$rec['id'].'</td>';
+					echo '<td><a href="friends_friends.php?area_id='.$rec['id'].'">'.$rec['name'].'</a></td>';
+					echo '<td>'.$rec['friends_count'].'</td>';
+					echo '</tr>';
+				}
+			}
 		echo '</table>';
 		//3.データベースから切断する
 		$dbh=null;
+	?>
+
+
+
+
+	<?php
+	//3.SQL文で都道府県リストをデータベースから取得し、表示させる。
+	// $sql ='SELECT * FROM `areas`;';
+	// 	//FROMでメインのテーブルを指定。表を結合を左側or右側からを指定し、結合させるテーブルを指定。今回は不要
+	// 	//$sql .='FROM areas LEFT OUTER JOIN friends';
+	// 	//メインのテーブルと結合させるテーブルの何が一致しているのか明示。
+	// 	//$sql .= 'ON areas.id = friends.area_id.areas.id ';
+	// 	//
+	// 	//$sql .= 'GROUP BY areas.id';
+	// 	//echo $sql;
+	// 	$stmt = $dbh->prepare($sql);
+	// 	$stmt->execute();
+	// 	echo '<table style="border:1px;">';
+	// 	while(1){
+	// 		$rec = $stmt->fetch(PDO::FETCH_ASSOC);
+	// 		//var_dump($rec);
+	// 		if ($rec == false) {
+	// 			break;
+	// 		}
+	// 		echo '<tr>';
+	// 		echo '<td>'.$rec['id'].'</td>';
+	// 		echo '<td><a href="friends_friends.php?area_id='.$rec['id'].'">'.$rec['name'].'</a></td>';
+	// 		echo '</tr>';
+	// 	}
+	// 	echo '</table>';
+	// 	//3.データベースから切断する
+	// 	$dbh=null;
 	?>
 
 </body>
